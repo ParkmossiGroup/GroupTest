bffeb43fb0ea3825774818618ee24ad5448dcc13
jyooung <cherish.yooung@gmail.com>
2025-02-27 12:52:54 +0900

    Test: repository 연결

---
+++ src/main/java/com/sparta/grouptest/controller/TestConroller.java
@@ -0,0 +1,10 @@
+package com.sparta.grouptest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestConroller {
+    @GetMapping("api/test")
+    public String test() { return "레파지토리 연결"; }
+}
---
+++ src/test/java/com/sparta/grouptest/GroupTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.grouptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
