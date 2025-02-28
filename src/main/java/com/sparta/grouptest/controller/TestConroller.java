65cc7ac28e25b4c4608c702b4294897ae2a7648c
jyooung <cherish.yooung@gmail.com>
2025-02-28 12:38:04 +0900

    test3: 충돌 발생

--- src/main/java/com/sparta/grouptest/controller/TestConroller.java
+++ src/main/java/com/sparta/grouptest/controller/TestConroller.java
@@ -7,4 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestConroller {
     @GetMapping("api/test")
     public String test() { return "레파지토리 연결"; }
+
+    @GetMapping("api/test3")
+    public String test3() { return "test3: 충돌 발생"; }
 }
